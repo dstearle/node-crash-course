6ba34499e90b35ebc672344e7a4ff640f445dac0
dstearle <dallas.st.earle@gmail.com>
2021-01-22 15:27:18 -0800

    Began working on example two logic for creating class 'Person'

--- person.js
+++ person.js
@@ -1,6 +1,33 @@
-const person = {
-    name: 'Alexander Earle',
-    age: 1
+// Exaple One
+
+// const person = {
+//     name: 'Alexander Earle',
+//     age: 1
+// }
+
+// The export for the 'person' data object
+// module.exports = person;
+
+// Example Two
+
+class Person {
+
+    // Constructor that accepts the user name and age
+    constructor(name, age) {
+
+        this.name = name;
+        this.age = age;
+
+    }
+
+    // Function that greets the user with their name and age
+    greeting() {
+
+        console.log(`My name is ${this.name} and I am ${this.age}`)
+
+    }
+
 }
 
-module.exports = person;
+// The export for the class 'Person'
+module.exports = Person;
