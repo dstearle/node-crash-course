3ecbacd358bb22e2e59ded487f591d074c049959
dstearle <dallas.st.earle@gmail.com>
2021-01-22 15:39:04 -0800

    Implemented 'Person' class into example two

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const person = require('./person');
+// Exaple One
 
-console.log(person.name);
+// const person = require('./person');
+
+// console.log(person.name);
+
+// Exaple Two
+
+// Imports in the 'Person' class
+const Person = require('./person');
+
+// Applies the provided data to the 'Person' class
+const person1 = new Person('Alexander Earle', 1);
+
+// Runs the 'greeting' function in the 'Person' class
+person1.greeting();
