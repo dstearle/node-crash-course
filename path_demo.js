40443fc501e4060fac588371e81b15ebc544bc66
dstearle <dallas.st.earle@gmail.com>
2021-01-22 16:10:54 -0800

    Finished examples for path_demo.js

--- path_demo.js
+++ path_demo.js
@@ -11,3 +11,9 @@ console.log(path.basename(__filename));
 
 // Retrieves just the extension for the file (Ex: '.js' for 'path_demo.js')
 console.log(path.extname(__filename));
+
+// Create path object
+console.log(path.parse(__filename));
+
+// Concatenate paths
+console.log(path.join(__dirname, 'test', 'hello.html'));
