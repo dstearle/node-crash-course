4dceb941ffa89bd645a5c78a014cdf2c734ee560
dstearle <dallas.st.earle@gmail.com>
2021-01-25 11:38:35 -0800

    Created example that shows the cpu's core info in the console

--- reference/os_demo.js
+++ reference/os_demo.js
@@ -3,9 +3,14 @@ const os = require('os');
 // Example One
 
 // Displays the operating system platform of the device
-console.log(os.platform());
+// console.log(os.platform());
 
 // Example Two
 
 // Displays the CPU architecture
-console.log(os.arch());
+// console.log(os.arch());
+
+// Example Three
+
+// Displays the CPU core info
+console.log(os.cpus());
