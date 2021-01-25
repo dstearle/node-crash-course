f6c1454984bbb711a70040894adb95b033a464df
dstearle <dallas.st.earle@gmail.com>
2021-01-25 11:10:21 -0800

    Created function that appends text to an existing file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+// Example One
+
 // Create Folder
 // fs.mkdir(path.join(__dirname, 'test'), {}, (err) => {
 
@@ -12,13 +14,39 @@ const path = require('path');
 
 // });
 
+// Example Two
+
 // Create & Write To File
-fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 'This is a test...hello!', (err) => {
+// fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 'This is a test...hello!', (err) => {
+
+//     // Throws error message in console if an error is found
+//     if(err) throw err;
+
+//     // Informs user that a folder was created in the console
+//     console.log('File written to...')
+
+// });
+
+// Example Three
+
+// Create & Write To File
+fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 'This is a test...hello! ', (err) => {
 
     // Throws error message in console if an error is found
     if(err) throw err;
 
     // Informs user that a folder was created in the console
     console.log('File written to...')
+
+    // Appends new text to the existing file
+    fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), 'This is another test...good bye! ', (err) => {
+
+        // Throws error message in console if an error is found
+        if(err) throw err;
+    
+        // Informs user that a folder was created in the console
+        console.log('File written to...')
+    
+    });
 
 });
