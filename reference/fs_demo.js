946d3af233512d8eb57a1d9f8c5045334499cac3
dstearle <dallas.st.earle@gmail.com>
2021-01-25 11:15:01 -0800

    Created function that reads the content of a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -30,23 +30,36 @@ const path = require('path');
 // Example Three
 
 // Create & Write To File
-fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 'This is a test...hello! ', (err) => {
+// fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 'This is a test...hello! ', (err) => {
+
+//     // Throws error message in console if an error is found
+//     if(err) throw err;
+
+//     // Informs user that a folder was created in the console
+//     console.log('File written to...')
+
+//     // Appends new text to the existing file
+//     fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), 'This is another test...good bye! ', (err) => {
+
+//         // Throws error message in console if an error is found
+//         if(err) throw err;
+    
+//         // Informs user that a folder was created in the console
+//         console.log('File written to...')
+    
+//     });
+
+// });
+
+// Example Four
+
+// Read File
+fs.readFile(path.join(__dirname, 'test', 'hello.txt'), 'utf8', (err, data) => {
 
     // Throws error message in console if an error is found
     if(err) throw err;
 
-    // Informs user that a folder was created in the console
-    console.log('File written to...')
-
-    // Appends new text to the existing file
-    fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), 'This is another test...good bye! ', (err) => {
-
-        // Throws error message in console if an error is found
-        if(err) throw err;
-    
-        // Informs user that a folder was created in the console
-        console.log('File written to...')
-    
-    });
+    // Displays the data from the file in the console
+    console.log(data)
 
 });
