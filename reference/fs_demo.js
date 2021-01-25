0cc10bd6ec95def32b0ef365aa98c86b40b235ea
dstearle <dallas.st.earle@gmail.com>
2021-01-25 10:57:32 -0800

    Setup error catch for creating a folder function

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,4 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 // Create Folder
-fs.mkdir();
+fs.mkdir(path.join(__dirname, 'test'), {}, function(err){
+
+    // Throws error message in console if an error is found
+    if(err) throw err;
+
+});
