3eb92635d46067f222c2f9b089ddc334e5c523d6
dstearle <dallas.st.earle@gmail.com>
2021-01-25 11:00:45 -0800

    Successfully created a folder using 'Create Folder' function

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 // Create Folder
-fs.mkdir(path.join(__dirname, 'test'), {}, function(err){
+fs.mkdir(path.join(__dirname, 'test'), {}, (err) => {
 
     // Throws error message in console if an error is found
     if(err) throw err;
+
+    // Informs user that a folder was created in the console
+    console.log('Folder created...')
 
 });
