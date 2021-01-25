80b8961ff80db8dfd8a841513f13d28a5be30ad3
dstearle <dallas.st.earle@gmail.com>
2021-01-25 13:25:54 -0800

    Created Logger class to be exported where needed

--- logger.js
+++ logger.js
@@ -2,4 +2,19 @@ const EventEmitter = require('events');
 const uuid = require('uuid');
 
 // Prints out a generated ID to the console
-console.log(uuid.v4());
+// console.log(uuid.v4());
+
+// A class that can be exported elsewhere
+class Logger extends EventEmitter {
+
+    log(msg) {
+
+        // Call Event
+        this.emit('message', { id: uuid.v4(), msg });
+
+    }
+
+}
+
+// Exports the Logger class
+module.exports = Logger;
